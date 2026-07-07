ef5c024275bd76c07cc4f751f821f82dae6d4cbd
Hakito_dev <vvv55019@gmail.com>
2026-07-08 02:49:13 +0500

    Init

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    CLIENT_ID: '1107688235485896854',
+
+    SERVERS: {
+        '1103415582101098638': {
+            channelId: '1103415582101098641',
+            welcomeMessage: 'Привет, {user}!\nДля общения авторизуйся в <#1125863671655051274>',
+            leaveMessage: 'Прощай, {user}'
+        },
+
+        '1137847512565301319': {
+            channelId: '1137847513546764330',
+            welcomeMessage: 'Привет, {user}!\nРад, что ты зашел к нам',
+            leaveMessage: 'Прощай, {user}'
+        },
+
+        '930865164000038943': {
+            channelId: '930907935176028200',
+            welcomeMessage: '{user},\nДля общения авторизуйся в\nTo communicate, log in to\n<#943053808781651989>',
+            leaveMessage: 'Прощай, {user}'
+        }
+    }
+};
