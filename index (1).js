140138f1cf80e3984a7a82ea4f71df0a10c3599a
Hakito_dev <vvv55019@gmail.com>
2026-07-08 02:32:22 +0500

    Init

---
+++ index (1).js
@@ -0,0 +1,143 @@
+const { Client, GatewayIntentBits, EmbedBuilder, REST, Routes, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('authpanel')
+    .setDescription('Аунтификация')
+    .addRoleOption(option =>
+      option.setName('role')
+        .setDescription('Роль для выдачи')
+        .setRequired(true)
+    )
+].map(c => c.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+const CLIENT_ID = '1107688235485896854';
+
+async function deploy() {
+  await rest.put(
+    Routes.applicationCommands(CLIENT_ID),
+    { body: commands }
+  );
+}
+
+deploy();
+
+const CONFIG = {
+  '1103415582101098638': { // bots support
+    channelId: '1103415582101098641',
+    welcomeMessage: 'Привет, {user}!\nДля общения авторизуйся в <#1125863671655051274>',
+    leaveMessage: 'Прощай, {user}'
+  },
+  '1137847512565301319': { // empire
+    channelId: '1137847513546764330',
+    welcomeMessage: 'Привет, {user}!\nРад, что ты зашел к нам',
+    leaveMessage: 'Прощай, {user}'
+  },
+  '930865164000038943': { // tea studio
+     channelId: '930907935176028200',
+     welcomeMessage: '{user},\nДля общения авторизуйся в \nTo communicate, log in to\n<#943053808781651989>',
+     leaveMessage: 'Прощай, {user}'
+   }
+};
+
+client.once('ready', () => {
+  console.log(`Bot is ready! Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  if (!interaction.guild) return;
+
+  if (interaction.commandName === 'authpanel') {
+    const role = interaction.options.getRole('role');
+
+    const button = new ButtonBuilder()
+      .setCustomId(`auth_${role.id}`)
+      .setLabel('🍀 Authentication')
+      .setStyle(ButtonStyle.Secondary);
+
+    const row = new ActionRowBuilder().addComponents(button);
+    const embed = new EmbedBuilder()
+    .setTitle('Authentication')
+    .setDescription('To communicate, click the button below');
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [row]
+    });
+  }
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isButton()) return;
+
+  if (interaction.customId.startsWith('auth_')) {
+    const roleId = interaction.customId.split('_')[1];
+    const role = interaction.guild.roles.cache.get(roleId);
+
+    if (!role) {
+      return interaction.reply({
+        content: '❌︙ERROR',
+        ephemeral: true
+      });
+    }
+
+    if (interaction.member.roles.cache.has(role.id)) {
+return interaction.reply({
+    content: '❌︙NOPE',
+    ephemeral: true
+  });
+    }
+
+    await interaction.member.roles.add(role);
+
+    return interaction.reply({
+      content: `🍀︙SUCCESS`,
+      ephemeral: true
+    });
+  }
+});
+
+client.on('guildMemberAdd', async (member) => {
+    const config = CONFIG[member.guild.id];
+    if (!config) return;
+    
+    const channel = member.guild.channels.cache.get(config.channelId);
+    if (!channel) return;
+    
+    const text = config.welcomeMessage.replace('{user}', `<@${member.id}>`);
+
+    const embed = new EmbedBuilder()
+    .setDescription(text);
+
+    await channel.send({ embeds: [embed] });
+});
+
+client.on('guildMemberRemove', async (member) => {
+    const config = CONFIG[member.guild.id];
+    if (!config) return;
+    
+    const channel = member.guild.channels.cache.get(config.channelId);
+    if (!channel) return;
+    
+    const text = config.leaveMessage.replace('{user}', `<@${member.id}>`);
+
+    const embed = new EmbedBuilder()
+    .setColor('#ED4245')
+    .setDescription(text)
+
+    await channel.send({ embeds: [embed] });
+});
+
+client.login(process.env.DISCORD_TOKEN);
