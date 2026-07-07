d12bc8ed9dbb1408dc17bf774ac11aadc1abe384
Hakito_dev <vvv55019@gmail.com>
2026-07-08 02:41:19 +0500

    Init
    Added functionality to deploy slash commands from command files.

--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
 
+const commands = [];
+const commandFiles = fs.readdirSync('./commands');
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+async function deploy() {
+  await rest.put(
+    Routes.applicationCommands('CLIENT_ID'),
+    { body: commands }
+  );
+
+  console.log('Slash commands deployed');
+}
+
+deploy();
