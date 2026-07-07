b2a500f85f0d19a8befd7166f90156ed3c3bb8bb
Hakito_dev <vvv55019@gmail.com>
2026-07-08 02:58:57 +0500

    Init

---
+++ Events/GuildMemberAdd.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder } = require('discord.js');
+const { SERVERS } = require('../config');
+
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(client, member) {
+        const config = SERVERS[member.guild.id];
+        if (!config) return;
+        const channel = member.guild.channels.cache.get(config.channelId);
+        if (!channel) return;
+        const embed = new EmbedBuilder()
+            .setDescription(
+                config.welcomeMessage.replace('{user}', `<@${member.id}>`)
+            );
+        await channel.send({
+            embeds: [embed]
+        });
+    }
+};
