9f917e1844ab1852e8614aee9f725603213ba14e
Hakito_dev <vvv55019@gmail.com>
2026-07-08 03:27:41 +0500

    Init

---
+++ Commands/Daily.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require("discord.js");
+const economy = require("../database/economy");
+
+const REWARD = 500;
+const COOLDOWN = 24 * 60 * 60 * 1000;
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName("daily")
+        .setDescription("Получить ежедневную награду"),
+
+    async execute(interaction) {
+
+        const user = await economy.getUser(interaction.user.id);
+
+        const now = Date.now();
+
+        if (now - user.daily < COOLDOWN) {
+
+            const hours = Math.ceil(
+                (COOLDOWN - (now - user.daily)) / 3600000
+            );
+
+            return interaction.reply({
+                content: `Следующая награда через ${hours} ч.`,
+                ephemeral: true
+            });
+
+        }
+
+        await economy.addBalance(interaction.user.id, REWARD);
+        await economy.setDaily(interaction.user.id, now);
+
+        interaction.reply({
+            content: `💰 Ты получил **${REWARD}** монет!`
+        });
+
+    }
+
+};
