24623cf56b41fed8d56fdaaba6c191adeae9e235
Hakito_dev <vvv55019@gmail.com>
2026-07-08 03:50:39 +0500

    Init

--- Deploy.js
+++ Deploy.js
@@ -6,11 +6,19 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands');
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+    try {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+        console.log(`✔ Загружена команда ${command.data.name}`);
+    } catch (err) {
+        console.error(`Ошибка в ${file}:`, err);
+    }
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+console.log(commandFiles);
+console.log(commands.map(c => c.name));
 
 async function deploy() {
   await rest.put(
