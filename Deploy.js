612f301745f8c3dfc07e80042bd09711ccac4d71
Hakito_dev <vvv55019@gmail.com>
2026-07-08 03:30:05 +0500

    Init

--- Deploy.js
+++ Deploy.js
@@ -2,6 +2,7 @@ const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 
 const commands = [];
+
 const commandFiles = fs.readdirSync('./commands');
 
 for (const file of commandFiles) {
