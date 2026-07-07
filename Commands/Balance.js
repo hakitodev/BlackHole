893d8ccaa1f375acc66238fd5e24286a3cbd994b
Hakito_dev <vvv55019@gmail.com>
2026-07-08 03:21:09 +0500

    Init

---
+++ Commands/Balance.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const economy = require("../database/economy");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("balance")
+        .setDescription("Посмотреть баланс")
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Пользователь")
+        ),
+    async execute(interaction) {
+        const member =
+            interaction.options.getUser("user") ??
+            interaction.user;
+        const user = await economy.getUser(member.id);
+        const embed = new EmbedBuilder()
+            .setTitle("💰 Баланс")
+            .setDescription(
+`**${member.username}**
+Наличные: **${user.balance}**
+Банк: **${user.bank}**`
+            );
+        await interaction.reply({
+            embeds: [embed]
+        });
+    }
+};
