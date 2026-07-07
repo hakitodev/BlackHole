0a6914879ac02ac2cfbdd62f791945b509ba477f
Hakito_dev <vvv55019@gmail.com>
2026-07-08 03:28:25 +0500

    Init

---
+++ Commands/Pay.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder } = require("discord.js");
+const economy = require("../database/economy");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName("pay")
+        .setDescription("Перевести деньги")
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("Получатель")
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option
+                .setName("amount")
+                .setDescription("Сумма")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        const target = interaction.options.getUser("user");
+        const amount = interaction.options.getInteger("amount");
+
+        if (amount <= 0)
+            return interaction.reply({
+                content: "Введите положительную сумму.",
+                ephemeral: true
+            });
+
+        if (target.bot)
+            return interaction.reply({
+                content: "Нельзя переводить ботам.",
+                ephemeral: true
+            });
+
+        if (target.id === interaction.user.id)
+            return interaction.reply({
+                content: "Нельзя перевести деньги самому себе.",
+                ephemeral: true
+            });
+
+        const sender = await economy.getUser(interaction.user.id);
+
+        if (sender.balance < amount)
+            return interaction.reply({
+                content: "Недостаточно средств.",
+                ephemeral: true
+            });
+
+        await economy.transfer(
+            interaction.user.id,
+            target.id,
+            amount
+        );
+
+        interaction.reply(
+            `💸 ${interaction.user} перевёл **${amount}** монет ${target}.`
+        );
+
+    }
+
+};
