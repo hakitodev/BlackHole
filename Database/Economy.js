c1b67134141688142e4bada6c5adcbc2e7948a4f
Hakito_dev <vvv55019@gmail.com>
2026-07-08 13:19:32 +0500

    Init

--- Database/Economy.js
+++ Database/Economy.js
@@ -1,11 +1,12 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
+const path = require("path");
 
 let db;
 
 async function initDatabase() {
     db = await open({
-        filename: "./Database/database.sqlite",
+        filename: path.join(__dirname, "database.sqlite"),
         driver: sqlite3.Database
     });
 
